61ebbc7c7b1a5d80337a1cd8d0658b3f24cf095a
itsm22051@gmail.com <himalpandey2002@gmail.com>
2025-05-06 09:08:10 +0545

    command line argument

---
+++ H_command_line_argument/a_java_program.java
@@ -0,0 +1,6 @@
+
+public class a_java_program{
+	public static void main(String[] args){
+		System.out.println("Hello Sanothimi:");
+	}
+}
--- H_command_line_argument/b_command_line_argument.java
+++
@@ -1,9 +0,0 @@
-public class b_command_line_argument{
-	public static void main(String[] args)
-	{
-		System.out.println("Hello Butwal!");
-		for(int i = 0; i<args.length; i++){
-		System.out.println(" " + args[i]);
-		}	
-	}
-}
